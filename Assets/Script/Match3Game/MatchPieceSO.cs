6cf70b3311e7ab3f9a1bdb09cb9e0f77c6da4dff
IceyFox006 <cgreenan@mail.bradley.edu>
2026-04-12 19:59:07 -0500

    PieceSetup

---
+++ Assets/Script/Match3Game/MatchPieceSO.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MatchPieceSO", menuName = "Scriptable Objects/Match3/Piece")]
+public class MatchPieceSO : ScriptableObject
+{
+    [Header("Data"), SerializeField]
+        private ElementSO _element;
+    [Header("Data"), SerializeField] 
+        private EffectTag[] _breakEffects;
+
+    [Header("Visuals"), SerializeField]
+        private Sprite _sprite;
+
+
+}
+
+public enum EffectTag
+{
+    BREAK_ROW = 001,
+    BREAK_COLUMN = 002,
+    BREAK_ALL_OF_ELEMENT = 003,
+
+}
