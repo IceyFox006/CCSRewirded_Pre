1f7991a3119e5ea6272f2462e9c2a5f8ce438495
IceyFox006 <cgreenan@mail.bradley.edu>
2026-04-14 19:32:33 -0500

    SelectMaterials

--- Assets/MarUtility/Script/ObjectEventSystem/ObjectButton.cs
+++ Assets/MarUtility/Script/ObjectEventSystem/ObjectButton.cs
@@ -6,6 +6,8 @@ public class ObjectButton : MonoBehaviour
 {
     [SerializeField]
         private bool _interactable = true;
+    private bool isHovered = false;
+    private bool isSelected = false;
 
     //Navigation
     [Header("Navigation")]
@@ -16,9 +18,9 @@ public class ObjectButton : MonoBehaviour
     [Header("Visual")]
     [SerializeField]
         private OBVisualType _visual;
-    [SerializeField, ShowIf("_visual", OBVisualType.MATERIAL)]
-        private OBVMaterial _materialVisual;
-
+    private OBVisual curVisual;
+    [SerializeField, ShowIf("_visual", OBVisualType.MATERIAL2D)]
+        private OBVMaterial2D _materialVisual;
 
     //Event
     [Header("Event")]
@@ -36,7 +38,29 @@ public class ObjectButton : MonoBehaviour
     #region GS
     public bool Interactable { get => _interactable; set => _interactable = value; }
     public OBNavigation Navigation { get => _navigation; set => _navigation = value; }
+    public bool IsSelected { get => isSelected; set => isSelected = value; }
 
+    #endregion
+    #region Initialize
+    private void Awake()
+    {
+        Initialize();
+    }
+    public void Initialize()
+    {
+        InitializeVisual();
+    }
+    private void InitializeVisual()
+    {
+        switch (_visual)
+        {
+            case OBVisualType.COLOR: break;
+            case OBVisualType.SPRITE: break;
+            case OBVisualType.ANIMATION: break;
+            case OBVisualType.MATERIAL2D: curVisual = _materialVisual; break;
+        }
+        curVisual.Initialize();
+    }
     #endregion
     #region Event
 
@@ -46,19 +70,32 @@ public class ObjectButton : MonoBehaviour
     }
     public void OnSelect()
     {
+        isSelected = true;
 
+        curVisual.ApplySelect();
     }
     public void OnDeselect()
     {
+        isSelected = false;
 
+        if (isHovered)
+            curVisual.ApplyHover();
+        else
+            curVisual.Reset();
     }
     public void OnHoverEnter()
     {
+        isHovered = true;
 
+        if (!isSelected)
+            curVisual.ApplyHover();
     }
     public void OnHoverExit()
     {
+        isHovered = false;
 
+        if (!isSelected)
+            curVisual.Reset();
     }
     #endregion
 }
@@ -96,6 +133,7 @@ public enum OBNavigationType
 [System.Serializable]
 public class OBVisual
 {
+    public virtual void Initialize() { }
     public virtual void Reset() { }
     public virtual void ApplyHover() { }
     public virtual void ApplySelect() { }
@@ -106,14 +144,16 @@ public enum OBVisualType
     COLOR,
     SPRITE,
     ANIMATION,
-    MATERIAL,
+    MATERIAL2D,
+    MATERIAL3D,
 }
 //---------------------------------------------------------------------------------------------------------------------
 [System.Serializable]
-public class OBVMaterial : OBVisual
+public class OBVMaterial2D : OBVisual
 {
     [SerializeField]
         private SpriteRenderer _renderer;
+    private Material defaultMaterial;
 
     [Header("Material")]
     [SerializeField, AllowNesting]
@@ -121,19 +161,23 @@ public class OBVMaterial : OBVisual
     [SerializeField, AllowNesting]
         private Material _select;
 
+    public override void Initialize()
+    {
+        defaultMaterial = _renderer.material;
+    }
     public override void Reset()
     {
-        base.Reset();
+        _renderer.material = defaultMaterial;
     }
 
     public override void ApplyHover()
     {
-        base.ApplyHover();
+        _renderer.material = _hover;
     }
 
     public override void ApplySelect()
     {
-        base.ApplySelect();
+        _renderer.material = _select;
     }
 }
 
--- Assets/MarUtility/Script/ObjectEventSystem/ObjectEventSystem.cs
+++ Assets/MarUtility/Script/ObjectEventSystem/ObjectEventSystem.cs
@@ -7,7 +7,7 @@ public class ObjectEventSystem : MonoBehaviour
     //Select
     [SerializeField]
         private ObjectButton _firstSelected;
-    private ObjectButton curHover = null;
+    public ObjectButton curHover = null;
     private List<ObjectButton> curSelected = new List<ObjectButton>();
 
     //Input
@@ -15,33 +15,56 @@ public class ObjectEventSystem : MonoBehaviour
         private bool _enableInputOnInitialize = true;
     [SerializeField]
         private InputActionAsset _inputActions;
+    [SerializeField]
+        private string moveActionPath = "MOVE";
     private InputAction move;
     private Vector2 moveDirection;
+    [SerializeField]
+        private string selectActionPath = "SELECT";
+    private InputAction select;
+    [SerializeField]
+        private string confirmActionPath = "CONFIRM";
+    private InputAction confirm;
 
+    private void Awake()
+    {
+        Initialize();
+    }
     public void Initialize()
     {
         curHover = _firstSelected;
+        _firstSelected.OnHoverEnter();
 
+        InitializeInput();
         if (_enableInputOnInitialize) 
             EnableInput();
 
     }
 
     #region Input
+    public void InitializeInput()
+    {
+        _inputActions.Enable();
+        move = _inputActions.FindAction(moveActionPath);
+        select = _inputActions.FindAction(selectActionPath);
+        confirm = _inputActions.FindAction(confirmActionPath);
+    }
     public void EnableInput()
     {
-        move = _inputActions.FindAction("MOVE");
-
         move.performed += Move_performed;
+        select.performed += Select_performed;
     }
+
     public void DisableInput()
     {
         move.performed -= Move_performed;
+        select.performed -= Select_performed;
     }
 
+    //Switches hover to button in direction.
     private void Move_performed(InputAction.CallbackContext obj)
     {
-        moveDirection = move.ReadValue<Vector2Int>();
+        moveDirection = move.ReadValue<Vector2>();
 
         //Switch Hover
         if (moveDirection == Vector2.up && CanMoveTo(curHover.Navigation.Up)) //Up
@@ -52,10 +75,19 @@ public class ObjectEventSystem : MonoBehaviour
             SwitchHover(curHover.Navigation.Left);
         else if (moveDirection == Vector2.right && CanMoveTo(curHover.Navigation.Right)) //Right
             SwitchHover(curHover.Navigation.Right);
+    }
 
+    //Select if button is not already selected, deselect if it is.
+    private void Select_performed(InputAction.CallbackContext obj)
+    {
+        if (!curHover.IsSelected)
+            curHover.OnSelect();
+        else
+            curHover.OnDeselect();
     }
     #endregion
 
+    //Switches which button is currently being hovered over.
     public void SwitchHover(ObjectButton bo)
     {
         curHover.OnHoverExit();
@@ -64,9 +96,10 @@ public class ObjectEventSystem : MonoBehaviour
     }
 
     #region Check
+    //Returns true if bo can be moved to.
     private bool CanMoveTo(ObjectButton bo)
     {
-        return (bo != null && !bo.Interactable);
+        return (bo != null && bo.Interactable);
     }
     #endregion
 }
